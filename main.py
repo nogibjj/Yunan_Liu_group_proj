8e027c9ee6a762eefb2d65148981de57e8659202
YunanLiu06 <112599210+YunanLiu06@users.noreply.github.com>
2022-12-13 22:42:59 +0000

    mod readme

--- main.py
+++ main.py
@@ -20,6 +20,33 @@ async def topfive():
     # return {result.to_json(orient='table')}
     return result.to_string()
 
+@app.get("/calc/{num1}/{operation}/{num2}")
+async def calc(num1: int, operation: str, num2: int):
+    """Simple calculation between 2 variables"""
+    
+    if operation != '+' and operation != '-' and operation != '*' and operation != 'd':
+        return {"Wrong operation input"}
+
+    elif operation == '+':
+        total = num1 + num2
+        return {"total": total}
+    
+    elif operation == "-":
+        total = num1 - num2
+        return {"total": total}
+
+    elif operation == "*":
+        total = num1 * num2
+        return {"total": total}
+
+    elif operation == "d":
+        if num2 == 0:
+            return {"The divisor cannot be zero"}
+        else:
+            total = num1/num2
+            return {"total": total}
+    
+
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, host="0.0.0.0")
